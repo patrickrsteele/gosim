edbd64370f17626674374f3335624dbd0ccee57e
Patrick Steele <steele.pat@gmail.com>
2013-03-16 15:16:42 -0400

    Updating before commit to github.

--- stats/student.go
+++ stats/student.go
@@ -17,80 +17,90 @@ func Student(d int) func(float64) float64 {
 	}
 }
 
-/* The cdf of the Student's t-distribution with d degrees of
-/* freedom. Compute using step size h. */
-func StudentCDF(d int, x float64, h float64) float64 {
-	// We know that this is a cdf with certain properties; exploit them
-	// to avoid unnecessary (innacurate) numerical integration
-	if x == 0 {
-		return .5
-	} else if x > 0 {
-		return .5 + goint.SimpsonIntegration(Student(d), 0, x, h)
-	}
+/* Returns the cdf of the Student's t-distribution with d degrees of
+/* freedom. The cdf requires a second argument h as the step size of
+/* the integration used to compute the cdf. */
+func StudentCDF(d int, h float64) func(float64) float64 {
+	pdf := Student(d)
 
-	// We have that x < 0
-	return .5 - goint.SimpsonIntegration(Student(d), x, 0, h)
+	return func(x float64) float64 {
+		// We know that this is a cdf with certain properties; exploit them
+		// to avoid unnecessary (innacurate) numerical integration
+		if x == 0 {
+			return .5
+		} else if x > 0 {
+			return .5 + goint.SimpsonIntegration(pdf, 0, x, h)
+		}
+
+		// We have that x < 0
+		return .5 - goint.SimpsonIntegration(pdf, x, 0, h)
+	}
 }
 
-/* Returns x such that F(x) = p, where F is the CDF of the Student's
-/* t-distribution with d degrees of freedom */
-func InvStudentCDF(d int, p float64, h float64) float64 {
+/* Returns an inverse of the function returned by StudentCDF with
+/* identical argumentss. */
+func InvStudentCDF(d int, h float64) func(float64) float64 {
 	const perr = 1e-5  // Acceptable percentile error
 	const berr = 1e-10 // Acceptable bounds error
 
-	var L, U float64
+	cdf := StudentCDF(d, h)
 
-	// Exploit known properties to avoid bisection, if we can
-	if p == 0 {
-		return math.Inf(-1)
-	} else if p == 1 {
-		return math.Inf(1)
-	} else if p == .5 {
-		return 0
-	}
+	return func(p float64) float64 {
 
-	// Choose bisection bounds somewhat intelligently
-	if p > .5 {
-		L = 0
-		U = 1
-		hh := h
-		for StudentCDF(d, U, hh) < p {
-			U *= 2
-			hh *= 2
-			if hh > 1 {
-				hh = 1
-			}
+		var L, U float64
+
+		// Exploit known properties to avoid bisection, if we can
+		if p == 0 {
+			return math.Inf(-1)
+		} else if p == 1 {
+			return math.Inf(1)
+		} else if p == .5 {
+			return 0
 		}
-	} else {
-		U = 0
-		L = -1
-		hh := h
-		for StudentCDF(d, L, hh) > p {
-			L *= 2
-			hh *= 2
-			if hh > 1 {
-				hh = 1
+
+		// Choose bisection bounds somewhat intelligently
+		if p > .5 {
+			L = 0
+			U = 1
+			hh := h
+			for StudentCDF(d, hh)(U) < p {
+				U *= 2
+				hh *= 2
+				if hh > 1 {
+					hh = 1
+				}
 			}
-		}
-	}
-
-	// Start bisection; halt when we're close enough to the proper percentile, or no progress is made
-	pL := StudentCDF(d, L, h)
-	pU := StudentCDF(d, U, h)
-	for pU-pL > 2*perr && U-L > berr {
-		M := (L + U) / 2
-		pM := StudentCDF(d, M, h)
-
-		if pM > p {
-			U = M
-			pU = pM
-		} else if pM < p {
-			L = M
-			pL = pM
 		} else {
-			return L
+			U = 0
+			L = -1
+			hh := h
+			for StudentCDF(d, hh)(L) > p {
+				L *= 2
+				hh *= 2
+				if hh > 1 {
+					hh = 1
+				}
+			}
 		}
-	}
 
-	return (L + U) / 2
+		// Start bisection; halt when we're close enough to the proper percentile, or no progress is made
+		pL := cdf(L)
+		pU := cdf(U)
+		for pU-pL > 2*perr && U-L > berr {
+			M := (L + U) / 2
+			pM := cdf(M)
+
+			if pM > p {
+				U = M
+				pU = pM
+			} else if pM < p {
+				L = M
+				pL = pM
+			} else {
+				return L
+			}
+		}
+
+		return (L + U) / 2
+	}
 }
