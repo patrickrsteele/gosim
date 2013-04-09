a262bf8348de6008343c3b50cb73e095bf407a1e
Patrick Steele <steele.pat@gmail.com>
2013-04-09 18:47:02 -0400

    Merged the control_variates.go and montecarlo.go files. This required updating the Trial type to require returning a slice of control variates, which we allow to be nil.

--- montecarlo.go
+++ montecarlo.go
@@ -1,6 +1,7 @@
 package gosim
 
 import (
+	"github.com/skelterjohn/go.matrix"
 	"gosim/stats"
 	"math"
 )
@@ -11,7 +12,9 @@ type Simulation interface {
 	Simulate(N int, alpha float64) *Estimate
 }
 
-type Trial func() float64
+/* Return an estimate of a parameter along with a slice of control variates;
+/* the slice can be nil. */
+type Trial func() (float64, []float64)
 
 type MonteCarloSimulation struct {
 	Simulation
@@ -24,13 +27,28 @@ func NewMonteCarlo(f Trial) *MonteCarloSimulation {
 
 /* Simulate N trials and report a (1 - alpha)-level confidence interval */
 func (m *MonteCarloSimulation) Simulate(N int, alpha float64) *Estimate {
-
-	values := make([]float64, N)
-	for i, _ := range values {
-		values[i] = m.F()
+	// We require at least one trial
+	if N <= 1 {
+		panic("N must be greater than 1")
 	}
 
-	return create_estimate(values, alpha)
+	n := float64(N)
+
+	// Run all trials, storing the results in ys and the controls in xs
+	ys := make([]float64, N)
+	xs := make([][]float64, N)
+	for i, _ := range ys {
+		ys[i], xs[i] = m.F()
+	}
+
+	d := len(xs[0])
+	// If there are no control variates, just do standard Monte Carlo
+	if d == 0 {
+		return create_estimate(ys, alpha)
+	}
+
+	// Otherwise return an estimate based off the control variates
+	return control_estimate(ys, xs, alpha)
 }
 
 func create_estimate(data []float64, alpha float64) *Estimate {
@@ -44,4 +62,68 @@ func create_estimate(data []float64, alpha float64) *Estimate {
 	coef *= math.Sqrt(variance / float64(len(data)))
 
 	return &Estimate{V: mean, C: &CI{Level: alpha, L: -coef, U: coef}}
+}
+
+/* Return a Monte Carlo estimate of the parameter, using the data in
+/* xs[i] as a control variate for observation ys[i]. */
+func control_estimate(ys []float64, xs [][]float64) *Estimate {
+	d := len(xs[0])
+
+	// Compute the means of the control variates
+	x_means := make([]float64, d)
+	for _, X := range xs {
+		for j, _ := range x_means {
+			x_means[j] += X[j]
+		}
+	}
+	for j, _ := range x_means {
+		x_means[j] /= n
+	}
+
+	// Compute the mean of the target
+	y_mean := Mean(ys)
+
+	// Compute the sample covariance of the control variates
+	Sx := matrix.MakeDenseMatrix(make([]float64, d*d), d, d)
+	for j := 0; j < d; j++ {
+		for k := 0; k < d; k++ {
+			entry := 0.0
+			for i, _ := range xs {
+				entry += xs[i][j]*xs[i][k] - n*x_means[j]*x_means[k]
+			}
+			entry /= n - 1
+
+			Sx.Set(j, k, entry)
+		}
+	}
+
+	// Compute the sample covariance of the control variates and the
+	// response
+	Sxy := matrix.MakeDenseMatrix(make([]float64, d), d, 1)
+	for j := 0; j < d; j++ {
+		entry := 0.0
+		for i, _ := range ys {
+			entry += xs[i][j]*ys[i] - n*x_means[j]*y_mean
+		}
+		entry /= n - 1
+
+		Sxy.Set(j, 0, entry)
+	}
+
+	// Compute the control variate weights
+	b := matrix.Product(matrix.Inverse(Sx), Sxy)
+
+	// Compute the offset estimates
+	yoff := make([]float64, N)
+	for i, _ := range yoff {
+		yoff[i] = ys[i]
+
+		for j := 0; j < d; j++ {
+			yoff[j] -= b.Get(j, 0) * (xs[i][j] - x_means[j])
+		}
+	}
+
+	// Compute the point estimate and sample variance of the offset
+	// values
+	return create_estimate(yoff, alpha)
 }
