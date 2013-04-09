a0cf91efb447303081200f13a1457286f3bca8ba
Patrick Steele <steele.pat@gmail.com>
2013-04-09 01:05:30 -0400

    Fixed ttable to reflect API changes, and removed a final case that was stressing the system.

--- stats/test/ttable.go
+++ stats/test/ttable.go
@@ -11,14 +11,16 @@ func main() {
 	dfs := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
 		11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
 		21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-		40, 60, 80, 100, 1000}
+		40, 60, 80, 100}
 
 	cls := []float64{.5, .75, .8, .85, .9, .95, .975, .99, .995, .999, .9995}
 	//cls := []float64{.999, .9995}
 
 	for _, df := range dfs {
+		icdf := stats.InvStudentCDF(df, 1e-5)
+		fmt.Printf("%d  ", df)
 		for _, cl := range cls {
-			fmt.Printf("%.3f   ", stats.InvStudentCDF(df, cl, 1e-5))
+			fmt.Printf("%.3f   ", icdf(cl))
 		}
 		fmt.Println()
 	}
